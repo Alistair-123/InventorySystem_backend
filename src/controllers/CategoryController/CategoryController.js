50f716ba30c9ac95140150819dced01849891a2e
alistair-123 <alixtairloberternos@gmail.com>
2026-01-14 10:32:51 +0800

    qweq

--- src/controllers/CategoryController/CategoryController.js
+++ src/controllers/CategoryController/CategoryController.js
@@ -30,9 +30,13 @@ export const getCategories = async (req, res) => {
     // Build query dynamically
     const query = {};
 
-    if (search) {
-      query.name = { $regex: search, $options: "i" }; // case-insensitive
+      if (search) {
+      query.$or = [
+        { categoryName: { $regex: search, $options: "i" } },
+        { categoryId: { $regex: search, $options: "i" } },
+      ];
     }
+
 
     const [categories, totalCategories] = await Promise.all([
       Category.find(query)
